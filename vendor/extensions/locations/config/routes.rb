f41589e0b23bbb46a82380b4ec4e5ff17a8af572
Dewald <dewald@perfectcircle.co.za>
2018-01-09 14:37:38 +0200

    g

--- vendor/extensions/locations/config/routes.rb
+++ vendor/extensions/locations/config/routes.rb
@@ -25,7 +25,7 @@ Refinery::Core::Engine.routes.append do
           end
         end
     end
-  end
+  end if defined?(::Refinery::Locations::Location)
 
   namespace :locations do
     resources :locations, :path => '', :only => [:index, :show]
